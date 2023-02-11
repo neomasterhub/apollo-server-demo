25ed826ed3a8df9091e3508c8b2cd5f0a068011b
Neomaster <the@neomaster.net>
2023-02-11 19:11:54 +0300

    Field as object (1-* rel).

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import movies from './data/movies.json' assert { type: 'json' };
+import countries from './data/countries.json' assert { type: 'json' };
 
 const typeDefs = `#graphql
     type Movie {
         id: ID!
         country_code: String
+        country: Country
         title: String!
         year: Int!
         cast: [String]!
         genres: [String]!
-    }   
+    }
+    type Country {
+        country_code: String!
+        name: String!
+    }
     type Query {
-        movies: [Movie]!
+        movies (year: Int, country_code: String): [Movie]!
     }
 `;
 
 const resolvers = {
+    Movie: {
+        country(parent, args, contextValue, info) {
+            return countries
+                .find(c => c.country_code === parent.country_code);
+        },
+    },
     Query: {
-        movies: () => movies,
+        movies(parent, args, contextValue, info) {
+            return movies
+                .filter(m => m.year === args.year && m.country_code == args.country_code);
+        },
     },
 };
 
